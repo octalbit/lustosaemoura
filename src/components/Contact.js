df3f64357dee29a99d17455a6812436e8366970a
jacksonbrunoss <oi.jbss@gmail.com>
2019-08-19 15:43:02 -0300

    Terminando page contatos

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,4 @@
-import React, { useState } from 'react';
-import Select from 'react-select';
-import axios from 'axios';
+import React from 'react';
 import './Contact.css';
 // import { Link } from 'react-router-dom';
 import SectionTitle from './SectionTitle';
@@ -8,38 +6,6 @@ import FormSendEmail from './FormSendEmail'
 import Logo from '../assets/img/logo.svg';
 
 export default function Contact() {
-
-    const [name,setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [areaAtuacao, setareaAtuacao] = useState('');
-    const [message, setMessage] = useState('');  
-    
-    function handleSubmit(event) {
-      if(email === '' || name === '' || telefone === '' || areaAtuacao === '' || message === ''){
-          alert('Preencha todos os campos ');
-      }
-      axios.post('https://calm-hamlet-35252.herokuapp.com/email', {
-        name,  
-        message,
-        telefone,
-        email,
-        areaAtuacao
-      })
-      alert('Mensagem enviada com sucesso !' );
-      event.preventDefault();
-    };
-    
-    const options = [
-        'Direito Empresarial',
-        'Direito da Familia',
-        'Direito Cívil',
-        'Direito do Trabalho',
-        'Direito Imobiliário',
-        'Direito Tributário',
-        'Direito Administrativo'
-    ];
-
     return (
         <section classname="lm-contact">
             <SectionTitle text="Solicite uma consulta" />
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,14 +1,34 @@
 import './Contact.css';
 import React from 'react';
+import TitlePage from '../../components/TitlePage';
+import FormSendEmail from '../../components/FormSendEmail';
 
-export default props => 
+export default props =>
 	<section className="lm-contact-page">
-		<div claName="container">
-			<div claName="lm-content-contact-page">
-				<div claName="colums">
-
-				
+		<TitlePage text="Contatos" />
+		<div className="container">
+			<div className="lm-content-contact-page">
+				<div className="columns">
+					<div className="column">
+						<div className="lm-content-contact">
+							<h3>Entre em contato com a gente</h3>
+							<p>Avenida Desembargador Amaral, n° 221, Centro, Amarante - PI, 64400-000<br />
+							Avenida Alberto Leal Nunes, n° 396, Centro, Regeneração - PI, 64490-000</p>
+							<p><strong>Tel: </strong>(86) 9 9851-3970 - Amarante<br />
+							<strong>Tel: </strong>(86) 9 9469-4942 - Regeneração</p>
+							<p><strong>E-mail: </strong>vicenteemoura@gmail.com</p>
+							<ul>
+								<li><i className="fab fa-facebook-f"></i></li>
+								<li><i className="fab fa-twitter"></i></li>
+								<li><i className="fab fa-instagram"></i></li>
+							</ul>
+							<p>
+								It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.
+							</p>
+						</div>
+					</div>
+					<div className="column"><FormSendEmail /></div>
 				</div>
-			</div>	
+			</div>
 		</div>
 	</section>
